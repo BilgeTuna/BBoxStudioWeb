3e51bda7bbfe23f7b695703d7cabc641fd94ca44
BilgeTuna <bilgetunagn@gmail.com>
2022-08-29 20:04:00 +0300

    Add project files.

---
+++ BBoxStudioWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BBoxStudioWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
